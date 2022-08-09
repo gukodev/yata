9df1acefc72df21fd9202ee6464f26bfbaa75a95
umgustavo <gugu.r.azevedo@gmail.com>
2022-08-09 16:13:17 -0300

    Add emoji favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -9,6 +9,10 @@ function MyApp({ Component, pageProps }) {
                 <title>YATA</title>
                 <meta charSet='utf-8' />
                 <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+                <link
+                    rel='icon'
+                    href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📝</text></svg>'
+                ></link>
             </Head>
             <Component {...pageProps} />
         </ChakraProvider>
