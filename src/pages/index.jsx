e0a3238e9eecd7b1aefa2c90336e12bcd17fa618
umgustavo <gugu.r.azevedo@gmail.com>
2022-08-09 16:11:05 -0300

    Add temp modal

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,48 @@
-import { Box, Container, Heading, Text, Button } from '@chakra-ui/react'
+import { useState } from 'react'
+import {
+    Box,
+    Container,
+    Heading,
+    Text,
+    Button,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+} from '@chakra-ui/react'
 
 export default function Home() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    function openModal() {
+        setIsOpen(true)
+    }
+
+    function handleModalClose() {
+        setIsOpen(false)
+    }
+
     return (
         <Box>
+            <Modal isOpen={isOpen} onClose={handleModalClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Create todo</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Text>Hello world!</Text>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button variant='ghost' mr={3} onClick={handleModalClose}>
+                            Cancel
+                        </Button>
+                        <Button colorScheme='purple'>Create</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
             <Container maxW='container.md' py={5}>
                 <Box textAlign='center' pb={5}>
                     <Heading>YATA</Heading>
