2790097f433d82b2cc2ec9286ff8d9ec3eef26fe
umgustavo <gugu.r.azevedo@gmail.com>
2022-08-11 16:22:31 -0300

    Add storage context provider

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,21 +1,24 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import Head from 'next/head'
+import { StorageContextProvider } from '../contexts/StorageContext'
 import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }) {
     return (
-        <ChakraProvider theme={theme}>
-            <Head>
-                <title>YATA</title>
-                <meta charSet='utf-8' />
-                <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-                <link
-                    rel='icon'
-                    href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📝</text></svg>'
-                ></link>
-            </Head>
-            <Component {...pageProps} />
-        </ChakraProvider>
+        <StorageContextProvider>
+            <ChakraProvider theme={theme}>
+                <Head>
+                    <title>YATA</title>
+                    <meta charSet='utf-8' />
+                    <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+                    <link
+                        rel='icon'
+                        href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📝</text></svg>'
+                    ></link>
+                </Head>
+                <Component {...pageProps} />
+            </ChakraProvider>
+        </StorageContextProvider>
     )
 }
 
