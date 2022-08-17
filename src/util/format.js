e973266145c6e87c5c893c9c89263660c2568f76
umgustavo <gugu.r.azevedo@gmail.com>
2022-08-17 15:59:05 -0300

    Add filterByMostRecent utility

--- src/util/format.js
+++ src/util/format.js
@@ -2,3 +2,11 @@ export function trailingZero(num) {
     num = parseInt(num, 10)
     return num < 10 ? '0' + num : num
 }
+
+export function filterByMostRecent(arr, field) {
+    return arr
+        .sort((a, b) => {
+            return new Date(a[field]) - new Date(b[field])
+        })
+        .reverse()
+}
